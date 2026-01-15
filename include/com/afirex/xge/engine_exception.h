72c0a38fb6712ec560156e025f730d8c32e8eeef
AFranklin78000 <AFranklin78000@gmail.com>
2026-01-15 02:32:34 -0600

    Added temporary simple engine exception thrower

---
+++ include/com/afirex/xge/engine_exception.h
@@ -0,0 +1,11 @@
+//
+// Created by afirex on 1/14/26.
+//
+
+#ifndef X_GAME_ENGINE_ENGINE_EXCEPTIONS_H
+#define X_GAME_ENGINE_ENGINE_EXCEPTIONS_H
+
+void ThrowException(const char *msg);
+void ThrowException(const char *msg_a, const char *msg_b);
+
+#endif //X_GAME_ENGINE_ENGINE_EXCEPTIONS_H
